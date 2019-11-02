162d22b34746516ca375fcb912f06c2397aaf5e6
Iratuã Júnior <iratuan@gmail.com>
2019-11-02 20:58:11 -0300

    Criação da classe aluno e testes de unidade

---
+++ FormacaoJava/java-collections/src/main/java/br/com/alura/Aluno.java
@@ -0,0 +1,20 @@
+package br.com.alura;
+
+public class Aluno {
+
+    private String nome;
+
+    public Aluno(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
