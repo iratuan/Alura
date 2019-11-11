de89940ee89d02dedbf58d024b9edc20a795d09f
Iratuã Júnior <iratuan@gmail.com>
2019-11-10 21:45:43 -0300

    Conclusão do projeto VRaptor

---
+++ FormacaoJava/java-maven/src/main/java/br/com/caelum/vraptor/controller/ProdutoController.java
@@ -0,0 +1,13 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Patch;
+
+@Controller
+public class ProdutoController {
+
+	@Patch("/produto/sobre")
+	public void sobre() {
+		
+	}
+}
