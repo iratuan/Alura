0426bc7bda466adccb212cc1a7dbf23ac9a1de58
Iratuã Júnior <iratuan@gmail.com>
2020-01-06 20:13:14 -0300

    Adição de projetos React

---
+++ treinamentos-react/joinApp/joinHeroesApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import Router from './routers/router' ;
+
+
+const App = () => {
+    return (
+       <Router />
+    );
+};
+
+export default App;
---
+++ treinamentos-react/joinApp/joinHeroesApp/src/pages/Dashboard.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {View, Text, ImageBackground, StyleSheet} from 'react-native';
+import Camera from '../components/Camera';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <ImageBackground
+        source={require('../../assets/background.png')}
+        style={{width: '100%', height: '100%'}}>
+        <View style={styles.viewContainer}>
+          <Camera />
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1,
+    padding: 30,
+  },
+});
+export default Dashboard;
