5b0fda764ef37876cedd7edb95bb0426e9a64a87
Iratuã Júnior <iratuajunior@MacBook-Pro-de-Iratua.local>
2020-02-15 14:00:24 -0300

    Modificacoes no arquivo de servico de estoque

--- AluraJAXWS/src/br/com/caelum/estoque/ws/EstoqueWs.java
+++ AluraJAXWS/src/br/com/caelum/estoque/ws/EstoqueWs.java
@@ -12,6 +12,7 @@ import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
@@ -20,10 +21,20 @@ public class EstoqueWS {
 
 	@WebMethod(operationName = "todosOsItens")
 	@WebResult(name = "itens")
-	public ListaItens getItens(@WebParam(name="filtros") Filtros filtros) {
+	public ListaItens getItens(@WebParam(name = "filtros") Filtros filtros) {
 		System.out.println("Chamando todosItens()");
 		List<Filtro> lista = filtros.getLista();
 		List<Item> itensResultado = dao.todosItens(lista);
 		return new ListaItens(itensResultado);
 	}
+
+	@WebMethod(operationName = "CadastrarItem")
+	@WebResult(name = "item")
+	public Item cadastrarItem(@WebParam(name = "tokenUsuario", header = true) TokenUsuario token,
+			@WebParam(name = "item") Item item) {
+		System.out.println("Cadastrando " + item + ", " + token);// imprimindo o token tbm
+		this.dao.cadastrar(item);
+		return item;
+	}
+
 }
