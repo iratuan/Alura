0846f259da5a5cda1af5537d7179fc3726baa3eb
Iratuã Júnior <iratuajunior@MacBook-Pro-de-Iratua.local>
2020-02-24 20:21:52 -0300

    Avancos no curso de REST

--- AluraREST/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
+++ AluraREST/loja/src/test/java/br/com/alura/loja/CarrinhoTest.java
@@ -1,5 +1,6 @@
 package br.com.alura.loja;
 
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -9,16 +10,16 @@ import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.thoughtworks.xstream.XStream;
 
-import br.com.alura.loja.modelo.Projeto;
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
+import junit.framework.Assert;
 
-public class ProjetoTest {
-	
+public class CarrinhoTest {
 	private HttpServer server;
 
 	@Before
@@ -30,33 +31,34 @@ public class ProjetoTest {
 	public void stopServer() {
 		server.stop();
 	}
-	
+
 	@Test
-    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
-        
-        
-        Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/projetos/1").request().get(String.class);
-		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
-		System.out.println(conteudo);
-		Assert.assertEquals("Minha loja", projeto.getNome());
-    }
-	
-	
-	@Test
-	public void testaAdicaoDeUmProjeto() {
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080");
-		Projeto projeto = new Projeto(2l, "BatCaverna", 2020);
-		String xml = projeto.toXML();
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		System.out.println(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testaAdicaoDeUmCarrinho() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314l, "Tablet", 999, 1));
+		carrinho.setRua("Rua Vergueiro");
+		carrinho.setCidade("São Paulo");
+		String xml = carrinho.toXML();
 		
 		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
-		Response response = target.path("/projetos").request().post(entity);
+		Response response = target.path("/carrinhos").request().post(entity);
 		Assert.assertEquals(201, response.getStatus());
 		
 		String location = response.getHeaderString("Location");
 		String conteudo = client.target(location).request().get(String.class);
-		Assert.assertTrue(conteudo.contains("BatCaverna"));
+		Assert.assertTrue(conteudo.contains("Rua Vergueiro"));
+		
 	}
 }
