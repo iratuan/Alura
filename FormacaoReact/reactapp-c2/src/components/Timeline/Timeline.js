55d9ce9ac034b618a8b5dc7e55f6ac111c7b237d
Iratuã Júnior <iratuajunior@MacBook-Pro-de-Iratua.local>
2020-03-12 20:59:48 -0300

    Organizacao das pastas da formacao JAVA

---
+++ FormacaoReact/reactapp-c2/src/components/Timeline/Timeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PhotoItem from "../PhotoItem";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div className="fotos container">
+        <PhotoItem />
+        <PhotoItem />
+      </div>
+    );
+  }
+}
