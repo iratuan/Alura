1e84cd4a25c443cbaab490232e4a8d1012390b7e
Iratuan Junior <iratuan@MacBook-Pro-2.local>
2020-12-01 12:56:42 -0300

    Curso de API REST SPRING BOOT

--- Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages={"br.com.alura.forum.controller"})
 public class ForumApplication {
 
 	public static void main(String[] args) {
