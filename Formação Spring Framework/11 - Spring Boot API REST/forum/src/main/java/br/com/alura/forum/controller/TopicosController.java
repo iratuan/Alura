539a5ddaf89ebc7d0df6d52a9f3a629698d62b81
Iratuã Júnior <iratuan@gmail.com>
2020-12-01 23:26:01 -0300

    Detalhamento de topico

--- Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -9,12 +9,14 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.DetalheTopicoDTO;
 import br.com.alura.forum.controller.dto.TopicoDTO;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
@@ -51,5 +53,11 @@ public class TopicosController {
 
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
+	}
+
+	@GetMapping("/{id}")
+	public DetalheTopicoDTO detalhar(@PathVariable("id") Long id) {
+		Topico topico = topicoRepository.getOne(id);
+		return new DetalheTopicoDTO(topico);
 	}
 }
