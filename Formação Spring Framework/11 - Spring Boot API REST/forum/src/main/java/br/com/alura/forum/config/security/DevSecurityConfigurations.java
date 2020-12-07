f652cae4f2f4bb880955fc81b8a958fb7dcc4a12
Iratan Júnior <iratuan@gmail.com>
2020-12-06 23:24:15 -0300

    Metade do curso de spring boot test

--- Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
+++ Formação Spring Framework/11 - Spring Boot API REST/forum/src/main/java/br/com/alura/forum/config/security/DevSecurityConfigurations.java
@@ -19,8 +19,8 @@ import br.com.alura.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
-@Profile("prod")
-public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+@Profile("dev")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private AutenticacaoService autenticacaoService;
@@ -47,15 +47,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").hasRole("MODERADOR")
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+		.antMatchers("/topicos/**").permitAll()		
+		.and().csrf().disable();
 	}
 	
 	
